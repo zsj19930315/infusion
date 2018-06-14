f4aed2407d732e704bd1b97ac60c486607c45f68
Administrator <Administrator@admin-4.fjiot.gov.cn>
2018-06-14 14:21:15 +0800

    infusion start

---
+++ src/main/java/cn/org/fjiot/infusion/InfusionApplication.java
@@ -0,0 +1,12 @@
+package cn.org.fjiot.infusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfusionApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/org/fjiot/infusion/InfusionApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.org.fjiot.infusion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfusionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
